25611cb986950ea5182ab1110cbb34bc34ffe4f1
Jamal A M <itsjamaal@outlook.com>
2022-05-27 22:18:04 +0400

    learn django

--- dailytrans/admin.py
+++ dailytrans/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Daily, MasterCategory, PaymentMode, Details
 
 # Register your models here.
+
+admin.site.register(PaymentMode)
+admin.site.register(Details)
+admin.site.register(MasterCategory)
+admin.site.register(Daily)
--- dailytrans/models.py
+++ dailytrans/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class MasterCategory(models.Model):
+    masterid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class PaymentMode(models.Model):
+    modeid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Details(models.Model):
+    detailsid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Daily(models.Model):
+    transid = models.AutoField(primary_key=True)
+    transdate = models.DateField()
+    transdetails = models.ForeignKey(Details, on_delete=models.CASCADE)
+    transcategory = models.ForeignKey(MasterCategory, on_delete=models.CASCADE)
+    transmode = models.ForeignKey(PaymentMode, on_delete=models.CASCADE)
+    transsubdetails = models.CharField(max_length=100, blank=False, null=False)
+    transamount = models.DecimalField(max_digits=7, decimal_places=2)
+    objects = models.Manager()
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s" % (
+            self.transid,
+            self.transdate,
+            self.transdetails,
+            self.transcategory,
+            self.transmode,
+            self.transsubdetails,
+            self.transamount)
